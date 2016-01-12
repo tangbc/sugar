967e4fd60f2392d44127a4478dfd4b75530af9fc
TANG <40519806@qq.com>
2016-01-12 18:27:47 +0800

    Update：完成observe模块，完善了object和array的监测

--- examples/demo/index.js
+++ examples/demo/index.js
@@ -4,39 +4,105 @@ require([
 ], function(sugar, Observer) {
 	var $ = sugar.jquery;
 
-	var test = {
-		'okc': {
-			'rwb': true,
-			'kdr': '35',
-			'sib': {
-				'blr': 12
-			}
-		}
-	}
+	// var obj = {
+	// 	'okc': {
+	// 		'rwb': true,
+	// 		'kdr': '35',
+	// 		'sib': {
+	// 			'blr': 12
+	// 		}
+	// 	}
+	// }
 
-	var ob = new Observer(test, function (name, newVal, oldVal) {
+	// new Observer(obj, function (name, newVal, oldVal, tar) {
+	// 	console.log(name, newVal, oldVal);
+	// });
+
+
+	// obj.okc.rwb = {
+	// 	'number': 0
+	// }
+
+	// obj.okc.rwb = {
+	// 	'number': 12
+	// }
+
+	// obj.okc.rwb = 12;
+
+	// obj.okc.sib.blr = {
+	// 	'hehe': 0
+	// }
+
+	// obj.okc.sib.blr = 100002;
+
+
+
+
+
+
+	// deep array ---------------------
+	var arr = [
+		{a: ['kb','sc','kd']},
+		['b1',['b21', 'b22'], ['b31', 'b32']],
+		{c: 3}
+	];
+
+	var foo = function foo(name, newVal, oldVal, tar) {
 		console.log(name, newVal, oldVal);
-	});
-	ob.init();
-
-
-	test.okc.rwb = {
-		'number': 0
 	}
 
-	test.okc.rwb = {
-		'number': 12
-	}
+	new Observer(arr, foo);
 
-	test.okc.rwb = 12;
+	// arr[0].a[2] = 'tang'; // 0*a*2 tang kd
 
-	test.okc.sib.blr = {
-		'hehe': 0
-	}
+	// arr[1][0] = 'b11'; // 1*0
 
-	// test.okc.sib.blr = 100002;
+	// arr[1].push('b3'); // push-1
 
-	// console.log(test)
+	arr[1][2].push('b33');
+
+	arr[0].a = 'worries';
+
+
+
+	// shallow array -------------------
+	// var arr = [1,2,3];
+
+	// var foo = function foo(name, newVal, oldVal, target) {
+	// 	console.log(name, newVal, oldVal);
+	// }
+
+	// new Observer(arr, foo);
+
+	// arr.push(4);
+
+
+
+	/* -------- deep object ----------*/
+	// var obj = {
+	// 	a: {
+	// 		b: {
+	// 			c: ['xxxx', 'yyyy', {
+	// 				d: 'ddd'
+	// 			}]
+	// 		}
+	// 	},
+	// 	b: {
+	// 		b: {
+	// 			b: 'old'
+	// 		}
+	// 	}
+	// }
+
+	// new Observer(obj, function(p, n, o) {
+	// 	console.log(p, n, o);
+	// });
+
+	// obj.b.b.b = 'new';
+
+	// obj.a.b.c[0] = 'XXX';
+
+	// obj.a.b.c[2].d = 'DDD';
 
 
 	var MainPage = sugar.Container.extend({
--- src/mvvm-observe.js
+++ src/mvvm-observe.js
@@ -7,127 +7,104 @@ define(['./util'], function(util) {
 	 * 对象变化监测类
 	 * @param  {Object|Array}  object    [对象或数组]
 	 * @param  {Function}      callback  [变化回调函数]
-	 * @param  {Object}        context   [<可选>执行上下文]
+	 * @param  {Object}        context   [执行上下文]
 	 */
 	function Observer(object, callback, context) {
+		var isArray = util.isArray(object);
+		var isObject = util.isObject(object);
+
+		if (!isObject && !isArray) {
+			util.error('object must be a type of Object or Array: ', object);
+			return;
+		}
+		if (!util.isFunc(callback)) {
+			util.error('callback must be a type of Function: ', callback);
+			return;
+		}
+
 		this._object = object;
 		this._callback = callback;
-		this._context = context || util.WIN;
+		this._context = context || util.GLOBAL;
+
+		/**
+		 * 监测的对象集合，包括一级和嵌套对象
+		 * @type  {Array}
+		 */
+		this.observers = [object];
+
+		/**
+		 * 监测的对象副本，存储旧值
+		 * @type  {Object}
+		 */
+		this.valuesMap = {
+			'0': util.copy(object)
+		};
+
+		/**
+		 * 需要重写的Array方法
+		 * @type  {Array}
+		 */
+		this.fixArrayMethods = 'push|pop|shift|unshift|splice|sort|reverse'.split('|');
+
+		/**
+		 * 属性层级分隔符
+		 * @type  {String}
+		 */
+		this.separator = '*';
+
+		// 处理数组
+		if (isArray) {
+			this.observeArray(object);
+		}
+
+		// 处理对象
+		if (isObject) {
+			this.observeObject(object);
+		}
 	}
 	Observer.prototype = {
 		constructor: Observer,
 
-		init: function() {
-			var cb = this._callback;
-			var object = this._object;
-			var isArray = util.isArray(object);
-			var isObject = util.isObject(object);
-
-			if (!isObject && !isArray) {
-				util.error('object must be a type of Object or Array: ', object);
-				return;
-			}
-			if (!util.isFunc(cb)) {
-				util.error('callback must be a type of Function: ', cb);
-				return;
-			}
-
-			/**
-			 * 需要重写的Array方法
-			 * @type  {String}
-			 */
-			this.fixArrayMethods = 'push|pop|shift|unshift|splice|concat|sort|reverse'.split('|');
-
-			/**
-			 * 监测的对象集合，包括一级和嵌套的对象
-			 * @type  {Array}
-			 */
-			this.observers = [object];
-
-			/**
-			 * 监测的对象副本
-			 * @type  {Object}
-			 */
-			this.valuesMap = {
-				'0': util.copy(object)
-			};
-
-			// 处理数组
-			if (isArray) {
-				this.observeArray(object);
-				this.rewriteArrayMethod();
-			}
-
-			// 处理对象
-			if (isObject) {
-				this.observeObject(object);
-			}
-		},
-
 		/**
 		 * 监测数组
+		 * @param   {Array}   array   [监测的对象]
+		 * @param   {Array}   paths   [<可选>访问路径数组]
 		 */
-		observeArray: function(array) {
-			this.handlerProperties(array);
+		observeArray: function(array, paths) {
+			this.rewriteArrayMethods(array, paths);
+			this.handleProperties(array, paths);
 		},
 
 		/**
 		 * 监测对象
+		 * @param   {Object}  object  [监测的对象]
+		 * @param   {Array}   paths   [<可选>访问路径数组]
 		 */
-		observeObject: function(object) {
-			this.handlerProperties(object);
+		observeObject: function(object, paths) {
+			this.handleProperties(object, paths);
 		},
 
 		/**
 		 * 处理对象属性的监测和数据缓存
 		 * @param   {Object|Array}  object  [对象或数组]
+		 * @param   {Array}         paths   [<可选>访问路径数组]
 		 */
-		handlerProperties: function(object) {
+		handleProperties: function(object, paths) {
 			util.each(object, function(value, property) {
+				// 路径副本
+				var copies = paths && paths.slice(0);
+				if (copies) {
+					copies.push(property);
+				}
+				else {
+					copies = [property];
+				}
+
 				this.setCacheProps(object, value, property)
-					.bindWatching(object, property);
+				.bindWatching(object, copies);
 			}, this);
 
 			return this;
-		},
-
-		/**
-		 * 获取target在源对象上的访问路径
-		 * @param   {Object}  target    [目标对象]
-		 * @param   {String}  property  [属性]
-		 * @return  {String}            [访问路径,以*分割层级]
-		 */
-		getPaths: function(target, property) {
-			var last, paths = [];
-			var srcObj = this._object;
-			var value = target[property];
-
-			var getPropertyPath = function _getPropertyPath(obj) {
-				util.each(obj, function(val, pro) {
-					// 目标匹配
-					if (val === value && pro === property) {
-						last = pro;
-						return false;
-					}
-					// 嵌套对象递归调用
-					else if (util.isObject(val)) {
-						paths.push(pro);
-						_getPropertyPath(val);
-					}
-				});
-			}
-
-			// 一级属性
-			if (util.has(property, srcObj)) {
-				paths.push(property);
-			}
-			// 嵌套属性
-			else {
-				getPropertyPath(srcObj);
-				paths.push(last);
-			}
-
-			return paths.join('*');
 		},
 
 		/**
@@ -139,7 +116,7 @@ define(['./util'], function(util) {
 		getCacheProps: function(object, property) {
 			var index = this.observers.indexOf(object);
 			var value = (index === -1) ? null : this.valuesMap[index];
-			return value ? value[property] : null;
+			return value ? value[property] : value;
 		},
 
 		/**
@@ -159,7 +136,7 @@ define(['./util'], function(util) {
 				observers.push(object);
 				valuesMap[oleng] = util.copy(object);
 			}
-			// 记录存在，重新对property赋值
+			// 记录存在，重新对object[property]赋值
 			else {
 				valuesMap[index][property] = value;
 			}
@@ -168,83 +145,107 @@ define(['./util'], function(util) {
 		},
 
 		/**
-		 * 对指定属性绑定监测
-		 * @param   {Object}  object    [对象]
-		 * @param   {String}  property  [属性]
+		 * 对属性绑定监测方法
+		 * @param   {Object}  object  [对象]
+		 * @param   {Array}   paths   [访问路径数组]
 		 */
-		bindWatching: function(object, property) {
-			var value = object[property];
-			var path = this.getPaths.apply(this, arguments);
+		bindWatching: function(object, paths) {
+			var prop = paths[paths.length - 1];
 
+			// 定义object的getter和setter
+			Object.defineProperty(object, prop, {
+				get: (function getter() {
+					return this.getCacheProps(object, prop);
+				}).bind(this),
+
+				set: (function setter() {
+					var newValue = arguments[0];
+					var oldValue = this.getCacheProps(object, prop);
+
+					this.setCacheProps(object, newValue, prop)
+					.triggerChange(paths.join(this.separator), newValue, oldValue, object);
+				}).bind(this)
+			});
+
+			var value = object[prop];
 			// 嵌套数组
 			if (util.isArray(value)) {
-				this.observeArray(value, index);
+				this.observeArray(value, paths);
 			}
 
 			// 嵌套对象
 			if (util.isObject(value)) {
-				this.observeObject(value, property);
+				this.observeObject(value, paths);
 			}
-
-			Object.defineProperty(object, property, {
-				get: (function getter() {
-					return this.getCacheProps(object, property);
-				}).bind(this),
-				set: (function setter() {
-					var newValue = arguments[0];
-					var oldValue = this.getCacheProps(object, property);
-
-					this.setCacheProps(object, newValue, property)
-						.triggerChange(path, newValue, oldValue, object);
-				}).bind(this)
-			});
 		},
 
 		/**
 		 * 重写Array方法
+		 * @param   {Array}  array  [目标数组]
+		 * @param   {Array}  paths  [<可选>访问路径数组]
 		 */
-		rewriteArrayMethod: function() {
-			var array = this._object;
+		rewriteArrayMethods: function(array, paths) {
+			var self = this;
 			var arrayProto = util.AP;
+			var arrayMethods = Object.create(arrayProto);
+			var path = paths && paths.join(self.separator);
 
 			util.each(this.fixArrayMethods, function(method) {
-				array[method] = (function reDefine() {
-					var args = util.argumentsToArray(arguments);
-					var oldResult = this.clearArray(array);
-					var result = arrayProto[method].apply(array, args);
-					var newResult = this.clearArray(array);
+				var original = arrayProto[method];
+				self.modifyProperty(arrayMethods, method, function redefineArrayMethod() {
+					var i = arguments.length, result;
+					var args = new Array(i);
+					while (i--) {
+						args[i] = arguments[i];
+					}
+					result = original.apply(this, args);
 
-					// 重新处理下标
-					this.handlerProperties(array);
-
-					this.triggerChange(method, newResult, oldResult, array);
+					self.triggerArrayMethod(method, path, this);
+					// 重新监测
+					self.observeArray(this);
 
 					return result;
-				}).bind(this)
-			}, this);
-		},
-
-		/**
-		 * 返回纯下标形式的数组(不引用)
-		 * @param   {Array}  array  [原数组]
-		 * @return  {Array}         [结果数组]
-		 */
-		clearArray: function(array) {
-			var ret = [];
-			util.each(array, function(value, index) {
-				ret[index] = value;
+				});
 			});
-			return ret;
+
+			array.__proto__ = arrayMethods;
+
+			return this;
 		},
 
 		/**
-		 * 触发对象属性值变化回调
-		 * @param   {String}         property  [属性名]
+		 * 修改target的property属性
+		 * @param   {Object|Array}  target    [数组或对象]
+		 * @param   {String}        property  [属性或下标]
+		 * @param   {Mix}           value     [修改值]
+		 */
+		modifyProperty: function(target, property, value) {
+			Object.defineProperty(target, property, {
+				'value'       : value,
+				'enumerable'  : false,
+				'writable'    : true,
+				'configurable': true
+			});
+		},
+
+		/**
+		 * 触发array的操作方法
+		 * @param   {String}  method  [操作方法]
+		 * @param   {Array}   path    [访问路径,undefined则为操作顶级数组]
+		 * @param   {Array}   array   [操作数组]
+		 */
+		triggerArrayMethod: function(method, path, array) {
+			this._callback.apply(this._context, ['Array:' + method, path, array]);
+		},
+
+		/**
+		 * 触发object变化回调
+		 * @param   {String}         paths     [变更路径]
 		 * @param   {Mix}            newValue  [新值]
 		 * @param   {Mix}            oldValue  [旧值]
-		 * @param   {Object|Array}   target    [原值]
+		 * @param   {Object|Array}   target    [变化对象]
 		 */
-		triggerChange: function(property, newValue, oldValue, target) {
+		triggerChange: function(paths, newValue, oldValue, target) {
 			this._callback.apply(this._context, arguments);
 		}
 	}
