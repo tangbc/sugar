7702f08e5e03d1f35bd2b9188f54fc4b11d1d4cb
TANG <40519806@qq.com>
2016-05-17 23:06:04 +0800

    Update version1.2.0

--- dist/sugar.js
+++ dist/sugar.js
@@ -1,5 +1,5 @@
 /*!
- * sugar.js v1.0.4
+ * sugar.js v1.2.0
  * (c) 2016 TANG
  * https://github.com/tangbc/sugar
  * released under the MIT license.
@@ -500,57 +500,6 @@ return /******/ (function(modules) { // webpackBootstrap
 		up.getKeyValue = function(expression, both) {
 			var array = expression.split(':');
 			return both ? array : array.pop();
-		}
-
-		/**
-		 * 分解字符串函数参数
-		 * @param   {String}  expression
-		 * @return  {Array}
-		 */
-		up.stringToParams = function(expression) {
-			var ret, params, func;
-			var exp = this.removeSpace(expression);
-			var matches = exp.match(/(\(.*\))/);
-			var result = matches && matches[0];
-
-			// 有函数名和参数
-			if (result) {
-				params = result.substr(1, result.length - 2).split(',');
-				func = exp.substr(0, exp.indexOf(result));
-				ret = [func, params];
-			}
-			// 只有函数名
-			else {
-				ret = [exp, params];
-			}
-
-			return ret;
-		}
-
-		/**
-		 * 字符 json 结构转为可取值的对象
-		 * @param   {String}  jsonString
-		 * @return  {Object}
-		 */
-		up.convertJson = function(jsonString) {
-			var info, props;
-			var string = jsonString.trim(), i = string.length;
-
-			if (/^\{.*\}$/.test(string)) {
-				info = {};
-				string = string.substr(1, i - 2).replace(/\s|'|"/g, '');
-				props = string.match(/[^,]+:[^:]+((?=,[\w_-]+:)|$)/g);
-
-				this.each(props, function(prop) {
-					var vals = this.getKeyValue(prop, true);
-					var name = vals[0], value = vals[1];
-					if (name && value) {
-						info[name] = value;
-					}
-				}, this);
-			}
-
-			return info;
 		}
 
 		/**
@@ -3587,67 +3536,19 @@ return /******/ (function(modules) { // webpackBootstrap
 
 		/**
 		 * 更新节点绑定事件的回调函数 realize v-on
-		 * @param   {DOMElement}  node
-		 * @param   {String}      evt          [事件名称]
-		 * @param   {Function}    func         [回调函数]
-		 * @param   {Function}    oldfunc      [旧回调函数，会被移除]
-		 * @param   {Array}       params       [参数]
-		 * @param   {String}      identifier   [对应监测字段/路径]
+		 * @param   {DOMElement}   node
+		 * @param   {String}       evt
+		 * @param   {Function}     callback
+		 * @param   {Boolean}      capture
+		 * @param   {Boolean}      unbind
 		 */
-		up.updateEvent = function(node, evt, func, oldfunc, params, identifier) {
-			var listeners = this.$listeners;
-			var modals, self, stop, prevent, capture = false;
-
-			// 支持 4 种事件修饰符 .self .stop .prevent .capture
-			if (evt.indexOf('.') !== -1) {
-				modals = evt.split('.');
-				evt = modals.shift();
-				self = modals && modals.indexOf('self') !== -1;
-				stop = modals && modals.indexOf('stop') !== -1;
-				prevent = modals && modals.indexOf('prevent') !== -1;
-				capture = modals && modals.indexOf('capture') !== -1;
-			}
-
-			if (oldfunc) {
-				dom.removeEvent(node, evt, listeners[identifier], capture);
-			}
-
-			if (util.isFunc(func)) {
-				// 缓存事件回调
-				listeners[identifier] = function _listener(e) {
-					var args = [];
-
-					// 是否限定只能在当前节点触发事件
-					if (self && e.target !== node) {
-						return;
-					}
-
-					// 组合事件参数
-					util.each(params, function(param) {
-						args.push(param === '$event' ? e : param);
-					});
-
-					// 未指定参数，则原生事件对象作为唯一参数
-					if (!args.length) {
-						args.push(e);
-					}
-
-					func.apply(this, args);
-
-					// 是否阻止冒泡
-					if (stop) {
-						e.stopPropagation();
-					}
-					// 是否阻止默认事件
-					if (prevent) {
-						e.preventDefault();
-					}
-				}
-
-				dom.addEvent(node, evt, listeners[identifier], capture);
+		up.updateEvent = function(node, evt, callback, capture, unbind) {
+			// 移除绑定
+			if (unbind) {
+				dom.removeEvent(node, evt, callback, capture);
 			}
 			else {
-				util.warn('The model: ' + identifier + '\'s value for using v-on must be a type of Function!');
+				dom.addEvent(node, evt, callback, capture);
 			}
 		}
 
@@ -4241,8 +4142,65 @@ return /******/ (function(modules) { // webpackBootstrap
 		__webpack_require__(2)
 	], __WEBPACK_AMD_DEFINE_RESULT__ = function(Parser, util) {
 
+		/**
+		 * 分解字符串函数参数
+		 * @param   {String}  funcString
+		 * @return  {Object}
+		 */
+		function stringToParams(funcString) {
+			var args, func;
+			var exp = util.removeSpace(funcString);
+			var matches = exp.match(/(\(.*\))/);
+			var result = matches && matches[0];
+
+			// 有函数名和参数
+			if (result) {
+				func = exp.substr(0, exp.indexOf(result));
+				args = '[' + result.substr(1, result.length - 2) + ']';
+			}
+			// 只有函数名
+			else {
+				func = exp;
+			}
+
+			return {
+				'func': func,
+				'args': args
+			}
+		}
+
+		/**
+		 * 字符 json 转为键值对象
+		 * @param   {String}  jsonString
+		 * @return  {Object}
+		 */
+		function convertJson(jsonString) {
+			var json, props;
+			var string = jsonString.trim(), i = string.length;
+
+			if (/^\{.*\}$/.test(string)) {
+				json = {};
+				string = string.substr(1, i - 2).replace(/\s/g, '');
+				props = string.match(/[^,]+:[^:]+((?=,[^:]+:)|$)/g);
+
+				util.each(props, function(prop) {
+					var vals = util.getKeyValue(prop, true);
+					var name = vals[0], value = vals[1];
+					if (name && value) {
+						name = name.replace(/(^'*)|('*$)|(^"*)|("*$)/g, '');
+						json[name] = value;
+					}
+				});
+			}
+
+			return json;
+		}
+
+
 		function Von(vm) {
 			this.vm = vm;
+			// 事件绑定回调集合
+			this.$listeners = {};
 			Parser.call(this);
 		}
 		var von = Von.prototype = Object.create(Parser.prototype);
@@ -4255,144 +4213,135 @@ return /******/ (function(modules) { // webpackBootstrap
 		 * @param   {String}      directive   [指令名称]
 		 */
 		von.parse = function(fors, node, expression, directive) {
-			var deps = this.getDeps(fors, expression);
-			var dir = util.removeSpace(directive);
-
-			// 单个事件 v-on:click
-			if (dir.indexOf(':') !== -1) {
-				this.parseSingle(node, expression, dir, fors, deps);
+			// 单个事件
+			if (directive.indexOf(':') !== -1) {
+				this.parseSingle.apply(this, arguments);
 			}
-			// 多个事件 v-on="{click: xxx, mouseenter: yyy, mouseleave: zzz}"
+			// 多个事件
 			else {
-				this.parseMulti(node, expression, fors, deps);
+				this.parseJson.apply(this, arguments);
 			}
 		}
 
 		/**
 		 * 解析单个 v-on:type
-		 * @param   {DOMElement}  node
-		 * @param   {String}      expression
-		 * @param   {String}      directive
-		 * @param   {Object}      fors
-		 * @param   {Array}       deps
 		 */
-		von.parseSingle = function(node, expression, directive, fors, deps) {
-			var vm = this.vm;
-			// 取值域
-			var scope = this.getScope(fors, expression);
+		von.parseSingle = function(fors, node, expression, directive) {
+			// 事件信息
+			var info = stringToParams(expression);
 			// 事件类型
 			var type = util.getKeyValue(directive);
-			// 事件信息
-			var info = util.stringToParams(expression);
 			// 事件取值字段名称
-			var field = info[0];
-			// 事件参数
-			var params = this.evalParams(fors, info[1]);
+			var field = info.func;
+			// 参数字符串
+			var paramString = info.args;
 
-			this.bindEvent(node, fors, scope, type, field, params, deps);
+			// 获取事件函数
+			var deps = this.getDeps(fors, field);
+			var scope = this.getScope(fors, field);
+			var getter = this.getEval(fors, field);
+			var func = getter.call(scope, scope);
+
+			// 绑定事件 & 参数求值
+			this.bindEvent(fors, node, field, type, func, paramString);
 
 			// 监测依赖变化，绑定新回调，旧回调将被移除
-			vm.watcher.watch(deps, function(path, last, old) {
-				this.update(node, type, last, old, params, path);
+			this.vm.watcher.watch(deps, function(path, lastCallback, oldCallback) {
+				// 解除绑定
+				this.update(node, type, this.$listeners[path], false, true);
+				// 绑定新回调
+				this.bindEvent(fors, node, path, type, lastCallback, paramString);
 			}, this);
 		}
 
 		/**
 		 * 解析多个 v-on=eventJson
-		 * @param   {DOMElement}  node
-		 * @param   {String}      expression
-		 * @param   {Object}      fors
-		 * @param   {Array}       deps
 		 */
-		von.parseMulti = function(node, expression, fors, deps) {
-			var vm = this.vm;
-			var cache = {}, jsonDeps = [], jsonAccess = [];
-			var events = util.convertJson(expression);
-
-			util.each(events, function(fn, ev) {
-				// 事件信息
-				var info = util.stringToParams(fn);
-				// 事件取值字段名称
-				var field = info[0] || fn;
-				// 事件参数
-				var params = this.evalParams(fors, info[1]);
-				// 访问路径
-				var access = deps.acc[deps.dep.indexOf(field)];
-				// 取值域
-				var scope = this.getScope(fors, field);
-
-				jsonDeps.push(field);
-				jsonAccess.push(access);
-				cache[access] = {
-					'type'  : ev,
-					'params': params,
-				}
-
-				this.bindEvent(node, fors, scope, ev, field, params, deps);
-			}, this);
-
-			// 监测依赖变化，绑定新回调，旧回调将被移除
-			vm.watcher.watch({
-				'dep': jsonDeps,
-				'acc': jsonAccess
-			}, function(path, last, old) {
-				var ev = cache[path];
-				this.update(node, ev.type, last, old, ev.params, path);
+		von.parseJson = function(fors, node, expression) {
+			util.each(convertJson(expression), function(exp, type) {
+				this.parseSingle(fors, node, exp, type);
 			}, this);
 		}
 
 		/**
 		 * 绑定一个事件
-		 * @param   {DOMElement}  node
 		 * @param   {Object}      fors
-		 * @param   {Object}      scope
-		 * @param   {String}      type
+		 * @param   {DOMElement}  node
 		 * @param   {String}      field
-		 * @param   {Array}       params
-		 * @param   {Array}       deps
+		 * @param   {String}      evt
+		 * @param   {Function}    func
+		 * @param   {String}      paramString
 		 */
-		von.bindEvent = function(node, fors, scope, type, field, params, deps) {
-			// 取值函数
-			var getter = this.getEval(fors, field);
-			// 事件函数
-			var func = getter.call(scope, scope);
-			// 访问路径，用于解绑
-			var access = deps.acc[deps.dep.indexOf(field)] || field;
+		von.bindEvent = function(fors, node, field, evt, func, paramString) {
+			var von = this;
+			var listeners = this.$listeners;
+			var identifier = fors && fors.access || field;
+			var modals, self, stop, prevent, capture = false;
 
-			this.update(node, type, func, null, params, access);
-		}
+			if (!util.isFunc(func)) {
+				return;
+			}
 
-		/**
-		 * 对函数参数求值
-		 * @param   {Object}  fors
-		 * @param   {Object}  scope
-		 * @param   {Array}   params
-		 * @return  {Array}
-		 */
-		von.evalParams = function(fors, params) {
-			var _params = [];
+			// 支持 4 种事件修饰符 .self .stop .prevent .capture
+			if (evt.indexOf('.') !== -1) {
+				modals = evt.split('.');
+				evt = modals.shift();
+				self = modals && modals.indexOf('self') !== -1;
+				stop = modals && modals.indexOf('stop') !== -1;
+				prevent = modals && modals.indexOf('prevent') !== -1;
+				capture = modals && modals.indexOf('capture') !== -1;
+			}
 
-			util.each(params, function(param) {
-				var p = param, exp, getter, scope;
+			// 事件代理函数
+			var eventProxy = function _eventProxy(e) {
+				var scope, getter, args = [];
 
-				if (util.isString(p) && p !== '$event') {
-					exp = this.replaceScope(p);
-
-					if (exp.indexOf('scope.') !== -1) {
-						scope = this.getScope(fors, p);
-						getter = this.createGetter(exp);
-						p = getter.call(scope, scope);
-					}
+				// 是否限定只能在当前节点触发事件
+				if (self && e.target !== node) {
+					return;
 				}
 
-				_params.push(p);
-			}, this);
+				if (paramString) {
+					// 取值域
+					scope = von.getScope(fors, paramString);
+					// 添加别名
+					scope.$event = e;
+					// 取值函数
+					getter = von.getEval(fors, paramString);
+					// 事件参数
+					args = getter.call(scope, scope);
+				}
 
-			return _params;
+
+				// 未指定参数，则原生事件对象作为唯一参数
+				if (!args.length) {
+					args.push(e);
+				}
+
+				func.apply(this, args);
+
+				// 是否阻止冒泡
+				if (stop) {
+					e.stopPropagation();
+				}
+				// 是否阻止默认事件
+				if (prevent) {
+					e.preventDefault();
+				}
+			}
+
+			listeners[identifier] = eventProxy;
+
+			// 添加绑定
+			this.update(node, evt, eventProxy, capture);
 		}
 
 		/**
 		 * 更新绑定事件
+		 * @param   {DOMElement}   node
+		 * @param   {String}       evt
+		 * @param   {Function}     callback
+		 * @param   {Boolean}      capture
 		 */
 		von.update = function() {
 			var updater = this.vm.updater;
