bf74b1d2f3558b42e08d1922493ef438ec6ad6f9
TANG <40519806@qq.com>
2016-06-25 13:12:07 +0800

    Add karma code coveage config.

--- build/karma.cover.config.js
+++ build/karma.cover.config.js
@@ -1,41 +1,51 @@
-// Use Karma to generate code coverage.
+// Use Karma to generate test code coverage.
 // https://github.com/karma-runner/karma-coverage
 var path = require('path');
 
 module.exports = function(config) {
 	config.set({
-		// src and test files
-		'files': [
-			// '../src/*.js',
-			// '../src/**/*.js',
-			'../test/units/specs/*.js',
-			'../test/units/specs/**/*.js'
-		],
+		// base path, that will be used to resolve files and exclude
+		'basePath': '',
 
+		// frameworks to use
 		'frameworks': ['jasmine'],
 
-		// coverage reporter generates the coverage
-		'reporters': ['progress', 'coverage'],
+		// start these browsers
+		'browsers': ['Chrome'],
 
+		// list of files to load in the browser
+		'files': [
+			'../test/units/index.js'
+		],
+
+		// list of preprocessors
 		'preprocessors': {
-			// source files, that you wanna generate coverage for
-			// do not include tests or libraries
-			// (these files will be instrumented by Istanbul)
-			'../src/*.js': ['webpack', 'sourcemap', 'coverage'],
-			'../src/**/*.js': ['webpack', 'sourcemap', 'coverage'],
-			'../test/units/specs/*.js': ['webpack', 'sourcemap'],
-			'../test/units/specs/**/*.js': ['webpack', 'sourcemap'],
+			'../test/units/index.js': ['webpack']
 		},
 
-		// optionally, configure the reporter
+		// test results reporter to use
+		// possible values: 'spec', 'dots', 'progress', 'junit', 'growl', 'coverage'
+		'reporters': ['progress', 'coverage'],
+
+		// set coverage report type and generate path
 		'coverageReporter': {
-			'dir': '../coverage/',
+			'dir': '../test/coverage/',
 			'reporters': [
-				{'type': 'lcov', 'subdir': '.' },
-				{'type': 'text-summary', 'subdir': '.' }
+				{'type': 'lcov', 'subdir': '.'},
+				{'type': 'text-summary', 'subdir': '.'}
 			]
 		},
 
+		// Continuous Integration mode
+		// if true, it capture browsers, run tests and exit
+		'singleRun': true,
+
+		// webpack middleware config
+		'webpackMiddleware': {
+			'noInfo': true
+		},
+
+		// webpack config
 		'webpack': {
 			'devtool': 'inline-source-map',
 			'resolve': {
@@ -45,38 +55,15 @@ module.exports = function(config) {
 				}
 			},
 			'module': {
-				'preLoaders': [{
-					'test': /\.js$/,
-					'exclude': [/node_modules/,/\.Spec.js$/],
-					'loader': 'istanbul-instrumenter-loader'
-				}]
+				// coverage post loader
+				'postLoaders': [
+					{
+						'test': /\.js$/,
+						'exclude': /test|node_modules/,
+						'loader': 'istanbul-instrumenter'
+					}
+				]
 			}
-		},
-
-		'webpackServer': {
-			'noInfo': true // prevent console spamming when running in Karma!
-		},
-
-		// 'plugins': [
-		// 	'karma-jasmine',
-		// 	'karma-chrome-launcher',
-		// 	'karma-webpack',
-		// 	'karma-sourcemap-loader',
-		// 	'karma-coverage'
-		// ],
-
-		// enable / disable colors in the output (reporters and logs)
-		'colors': true,
-
-		// level of logging
-		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-		'logLevel': config.LOG_INFO,
-
-		// enable / disable watching file and executing tests whenever any file changes
-		'autoWatch': false,
-
-		// Continuous Integration mode
-		// if true, Karma captures browsers, runs the tests and exits
-		'singleRun': true
+		}
 	});
 }
