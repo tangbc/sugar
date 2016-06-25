22592381d0c793725907704df5208ad7b5789695
TANG <40519806@qq.com>
2016-06-25 13:12:35 +0800

    Update webpack config

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,51 +1,24 @@
 /**
  * webpack config file
- * multi-complier example here:
  * https://github.com/webpack/webpack/tree/master/examples/multi-compiler
  */
 
 var webpack = require('webpack');
 var banner = require('./banner');
 
-var sugar = {
-	'entry' : './src/main/index',
-	'output': {
-		'path'         : './dist',
-		'library'      : 'Sugar',
-		'filename'     : 'sugar.js',
-		'libraryTarget': 'umd'
-	},
-	'plugins': [
-		new webpack.BannerPlugin(banner.sugar, {
-			'raw'      : false,
-			'entryOnly': false
-		})
-	]
+// https://github.com/mishoo/UglifyJS2#compressor-options
+// http://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
+var uglifyConifg = {
+	'compress': {
+		'warnings': false
+	}
+}
+// http://webpack.github.io/docs/list-of-plugins.html#bannerplugin
+var bannerConfig = {
+	'raw'      : false,
+	'entryOnly': false
 }
 
-var sugarUglify = {
-	'entry' : './src/main/index',
-	'output': {
-		'path'         : './dist',
-		'library'      : 'Sugar',
-		'filename'     : 'sugar.min.js',
-		'libraryTarget': 'umd'
-	},
-	'plugins': [
-		// https://github.com/mishoo/UglifyJS2#compressor-options
-		// http://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
-		new webpack.optimize.UglifyJsPlugin({
-			'compress': {
-				'warnings': false
-			}
-		}),
-		// http://webpack.github.io/docs/list-of-plugins.html#bannerplugin
-		new webpack.BannerPlugin(banner.sugar, {
-			'raw'      : false,
-			'entryOnly': false
-		})
-	]
-}
 
 var mvvm = {
 	'entry' : './src/mvvm/index',
@@ -56,13 +29,9 @@ var mvvm = {
 		'libraryTarget': 'umd'
 	},
 	'plugins': [
-		new webpack.BannerPlugin(banner.mvvm, {
-			'raw'      : false,
-			'entryOnly': false
-		})
+		new webpack.BannerPlugin(banner.mvvm, bannerConfig)
 	]
 }
-
 var mvvmUglify = {
 	'entry' : './src/mvvm/index',
 	'output': {
@@ -72,16 +41,36 @@ var mvvmUglify = {
 		'libraryTarget': 'umd'
 	},
 	'plugins': [
-		new webpack.optimize.UglifyJsPlugin({
-			'compress': {
-				'warnings': false
-			}
-		}),
-		new webpack.BannerPlugin(banner.mvvm, {
-			'raw'      : false,
-			'entryOnly': false
-		})
+		new webpack.optimize.UglifyJsPlugin(uglifyConifg),
+		new webpack.BannerPlugin(banner.mvvm, bannerConfig)
 	]
 }
 
-module.exports = [sugar, sugarUglify, mvvm, mvvmUglify];
+var sugar = {
+	'entry' : './src/main/index',
+	'output': {
+		'path'         : './dist',
+		'library'      : 'Sugar',
+		'filename'     : 'sugar.js',
+		'libraryTarget': 'umd'
+	},
+	'plugins': [
+		new webpack.BannerPlugin(banner.sugar, bannerConfig)
+	]
+}
+var sugarUglify = {
+	'entry' : './src/main/index',
+	'output': {
+		'path'         : './dist',
+		'library'      : 'Sugar',
+		'filename'     : 'sugar.min.js',
+		'libraryTarget': 'umd'
+	},
+	'plugins': [
+		new webpack.optimize.UglifyJsPlugin(uglifyConifg),
+		new webpack.BannerPlugin(banner.sugar, bannerConfig)
+	]
+}
+
+
+module.exports = [mvvm, mvvmUglify, sugar, sugarUglify];
