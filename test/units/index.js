617790292689bd80e5708530b388496215382a1c
TANG <40519806@qq.com>
2016-07-07 14:52:35 +0800

    Add detection for util.warn in every test spec

--- test/units/index.js
+++ test/units/index.js
@@ -1,2 +1,8 @@
+var util = require('src/util').default;
+
+beforeEach(function() {
+	spyOn(util, 'warn');
+});
+
 var testsContext = require.context(".", true, /_spec$/);
 testsContext.keys().forEach(testsContext);
