d4dd19580cc773dcac02e98b04b4d642752d1b07
TANG <40519806@qq.com>
2016-07-24 09:13:59 +0800

    Update sugar export.

--- src/main/index.js
+++ src/main/index.js
@@ -4,39 +4,4 @@ import util from '../util';
 import Module from './module';
 import Component from './component';
 
-/**
- * sugar 构造函数入口
- */
-function Sugar () {
-	/**
-	 * 工具方法
-	 * @type  {Object}
-	 */
-	this.util = util;
-
-	/**
-	 * Ajax
-	 * @type  {Object}
-	 */
-	this.ajax = ajax;
-
-	/**
-	 * 系统核心实例
-	 * @type  {Object}
-	 */
-	this.core = core;
-
-	/**
-	 * 基础模块类
-	 * @type  {Class}
-	 */
-	this.Module = Module;
-
-	/**
-	 * 视图组件基础模块
-	 * @type  {Class}
-	 */
-	this.Component = Component;
-}
-
-export default new Sugar();
+export default { ajax, core, util, Module, Component }
