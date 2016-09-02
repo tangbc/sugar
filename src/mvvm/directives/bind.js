2b5460f1e2cf0006e0c9bbb4a643bfb9940c1ccd
TANG <40519806@qq.com>
2016-09-02 20:07:20 +0800

    Add diff to bind.js

--- src/mvvm/directives/bind.js
+++ src/mvvm/directives/bind.js
@@ -1,6 +1,92 @@
 import Parser, { linkParser } from '../parser';
 import { setAttr, addClass, removeClass } from '../../dom';
-import { each, isString, isArray, isObject, diff, warn } from '../../util';
+import { each, isString, isArray, isObject, isEmptyObject, warn } from '../../util';
+
+/**
+ * 返回 contrastObject 相对于 referObject 的差异对象
+ * @param   {Object}  contrastObject  [对比对象]
+ * @param   {Object}  referObject     [参照对象]
+ * @return  {Object}
+ */
+function getUniqueObject (contrastObject, referObject) {
+	var unique = {};
+
+	each(contrastObject, function (value, key) {
+		var _diff, oldItem = referObject[key];
+
+		if (isObject(value)) {
+			_diff = getUniqueObject(value, oldItem);
+			if (!isEmptyObject(_diff)) {
+				unique[key] = _diff;
+			}
+		} else if (isArray(value)) {
+			var newArray = [];
+
+			each(value, function (nItem, index) {
+				var _diff;
+
+				if (isObject(nItem)) {
+					_diff = getUniqueObject(nItem, oldItem[index]);
+					newArray.push(_diff);
+				}
+				else {
+					// 新数组元素
+					if (oldItem.indexOf(nItem) < 0) {
+						newArray.push(nItem);
+					}
+				}
+			});
+
+			unique[key] = newArray;
+		} else {
+			if (value !== oldItem) {
+				unique[key] = value;
+			}
+		}
+	});
+
+	return unique;
+}
+
+/**
+ * 返回 contrastArray 相对于 referArray 的差异数组
+ * @param   {Array}  contrastArray  [对比数组]
+ * @param   {Array}  referArray     [参照数组]
+ * @return  {Array}
+ */
+function getUniqueArray (contrastArray, referArray) {
+	var uniques = [];
+
+	if (!isArray(contrastArray) || !isArray(referArray)) {
+		return contrastArray;
+	}
+
+	each(contrastArray, function (item) {
+		if (referArray.indexOf(item) < 0) {
+			uniques.push(item);
+		}
+	});
+
+	return uniques;
+}
+
+/**
+ * 返回两个比较值的差异
+ * 获取绑定 object 和 array 的更新差异
+ * @param   {Object|Array}  newTarget
+ * @param   {Object|Array}  oldTarget
+ * @return  {Object}
+ */
+function diff (newTarget, oldTarget) {
+	var isA = isArray(newTarget) && isArray(oldTarget);
+	var isO = isObject(newTarget) && isObject(oldTarget);
+	var handler = isO ? getUniqueObject : (isA ? getUniqueArray : null);
+
+	var after = handler && handler(newTarget, oldTarget) || newTarget;
+	var before = handler && handler(oldTarget, newTarget) || oldTarget;
+
+	return { after, before };
+}
 
 /**
  * 处理 styleObject, 批量更新元素 style
@@ -104,7 +190,7 @@ vbind.single = function (type, newValue, oldValue) {
 			this.handleStyle(newValue, oldValue);
 			break;
 		default:
-			this.handleAttr(type, newValue, oldValue);
+			this.handleAttr(type, newValue);
 	}
 }
 
@@ -175,9 +261,8 @@ vbind.handleStyle = function (newStyle, oldStyle) {
 /**
  * 更新处理 attribute
  * @param   {String}   attr
- * @param   {String}   newValue
- * @param   {String}   oldValue
+ * @param   {String}   value
  */
-vbind.handleAttr = function (attr, newValue, oldValue) {
-	setAttr(this.el, attr, newValue);
+vbind.handleAttr = function (attr, value) {
+	setAttr(this.el, attr, value);
 }
