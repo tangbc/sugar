4e22af07210abc776ab93a35540354b7796cf1a8
TANG <40519806@qq.com>
2016-09-02 20:01:26 +0800

    Remove diff from util.

--- src/util.js
+++ src/util.js
@@ -379,91 +379,9 @@ export function removeSpace (string) {
 }
 
 /**
- * 返回 contrastObject 相对于 referObject 的差异对象
- * @param   {Object}  contrastObject  [对比对象]
- * @param   {Object}  referObject     [参照对象]
- * @return  {Object}
+ * 挂载到 sugar 上的工具方法
+ * @param   {Object}
  */
-function getUniqueObject (contrastObject, referObject) {
-	var unique = {};
-
-	each(contrastObject, function (value, key) {
-		var _diff, oldItem = referObject[key];
-
-		if (isObject(value)) {
-			_diff = getUniqueObject(value, oldItem);
-			if (!isEmptyObject(_diff)) {
-				unique[key] = _diff;
-			}
-		} else if (isArray(value)) {
-			var newArray = [];
-
-			each(value, function (nItem, index) {
-				var _diff;
-
-				if (isObject(nItem)) {
-					_diff = getUniqueObject(nItem, oldItem[index]);
-					newArray.push(_diff);
-				}
-				else {
-					// 新数组元素
-					if (oldItem.indexOf(nItem) < 0) {
-						newArray.push(nItem);
-					}
-				}
-			});
-
-			unique[key] = newArray;
-		} else {
-			if (value !== oldItem) {
-				unique[key] = value;
-			}
-		}
-	});
-
-	return unique;
-}
-
-/**
- * 返回 contrastArray 相对于 referArray 的差异数组
- * @param   {Array}  contrastArray  [对比数组]
- * @param   {Array}  referArray     [参照数组]
- * @return  {Array}
- */
-function getUniqueArray (contrastArray, referArray) {
-	var uniques = [];
-
-	if (!isArray(contrastArray) || !isArray(referArray)) {
-		return contrastArray;
-	}
-
-	each(contrastArray, function (item) {
-		if (referArray.indexOf(item) < 0) {
-			uniques.push(item);
-		}
-	});
-
-	return uniques;
-}
-
-/**
- * 返回两个比较值的差异
- * 用于获取 v-bind 绑定 object 的更新差异
- * @param   {Object|Array}  newTarget
- * @param   {Object|Array}  oldTarget
- * @return  {Object}
- */
-export function diff (newTarget, oldTarget) {
-	var isA = isArray(newTarget) && isArray(oldTarget);
-	var isO = isObject(newTarget) && isObject(oldTarget);
-	var handler = isO ? getUniqueObject : (isA ? getUniqueArray : null);
-
-	var after = handler && handler(newTarget, oldTarget) || newTarget;
-	var before = handler && handler(oldTarget, newTarget) || oldTarget;
-
-	return { after, before };
-}
-
 export default {
 	each,
 	copy,
