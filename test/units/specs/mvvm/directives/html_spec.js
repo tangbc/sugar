02f92e4c5ef87b283c19abb24ebea7b4efbf74c7
TANG <40519806@qq.com>
2016-10-10 14:46:33 +0800

    Update test spec.

--- test/units/specs/mvvm/directives/html_spec.js
+++ test/units/specs/mvvm/directives/html_spec.js
@@ -83,46 +83,4 @@ describe("v-html >", function () {
 		expect(ps[0].innerHTML).toBe('plain text bbb');
 		expect(ps[1].innerHTML).toBe('<div>CC</div>');
 	});
-
-
-	it('mustache in v-for', function () {
-		element.innerHTML =
-			'<ul>' +
-				'<li v-for="item in items">' +
-					'<p class="pp" v-html="item.layout"></p>' +
-				'</li>' +
-			'</ul>'
-
-		var vm = new MVVM({
-			'view': element,
-			'model': {
-				'items': [
-					{'layout': '<i>aaa</i>'},
-					{'layout': '<b>bbb</b>'},
-					{'layout': '<span>ccc</span>'},
-				]
-			}
-		});
-		var items = vm.$data.items;
-		var ps = element.querySelectorAll('.pp');
-
-		expect(ps[0].innerHTML).toBe('<i>aaa</i>');
-		expect(ps[1].innerHTML).toBe('<b>bbb</b>');
-		expect(ps[2].innerHTML).toBe('<span>ccc</span>');
-
-		items[0].layout = '<h2>AAA</h2>';
-		expect(ps[0].innerHTML).toBe('<h2>AAA</h2>');
-
-		items[1].layout = 'plain text bbb';
-		expect(ps[1].innerHTML).toBe('plain text bbb');
-
-		items.$set(2, {'layout': '<div>CC</div>'});
-		ps = element.querySelectorAll('.pp');
-		expect(ps[2].innerHTML).toBe('<div>CC</div>');
-
-		items.$remove(items[0]);
-		ps = element.querySelectorAll('.pp');
-		expect(ps[0].innerHTML).toBe('plain text bbb');
-		expect(ps[1].innerHTML).toBe('<div>CC</div>');
-	});
 });
